273f088bf83beedb39ed3a7b52e09acb5b0f99d5
Franco Cragnolini <cragnolinifranco@gmail.com>
2022-08-24 14:43:19 -0300

    section 3

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Console } = require("console");
+const express = require("express");
+const path = require("path");
+
+// App de express
+const app = express();
+require("dotenv").config();
+
+// Node server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+
+require("./sockets/socket");
+
+// pathPublico
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.log("Servidor corriendo en puerto " + process.env.PORT);
+});
